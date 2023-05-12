d8d2b92555fbe8c8240bd20e98c9b6b6d2822a3d
GitGud123456 <98549374+GitGud123456@users.noreply.github.com>
2023-05-12 17:26:33 -0600

    updated

--- scriptv3.js
+++ scriptv3.js
@@ -9,7 +9,7 @@ let select = document.getElementById("mct");
 let btn = document.getElementById("btn");
 let clearbtn = document.getElementById("clear");
 clearbtn.addEventListener("click", clear);
-document.addEventListener("keyup", initialize);
+document.addEventListener("keyup", initialize());
 btn.addEventListener("click", makeRandom, initialize);
 let bhm = 0;
 let xL = 0;
@@ -47,30 +47,32 @@ function makeRandom() {
 function clear() {
   objects = [];
 }
-for (var i = 0; i < 500; i++) {
-  var x = Math.random() * canvas.width;
-  var y = Math.random() * canvas.height;
-  var vx = Math.random() * 4 - 2;
-  var vy = Math.random() * 4 - 2;
-  var m = Math.random() * 20;
-  var r = m / 2;
-  objects.push(new Object(x, y, vx, vy, m, r));
-}
+// for (var i = 0; i < 500; i++) {
+//   var x = Math.random() * canvas.width;
+//   var y = Math.random() * canvas.height;
+//   var vx = Math.random() * 4 - 2;
+//   var vy = Math.random() * 4 - 2;
+//   var m = Math.random() * 20;
+//   var r = m / 2;
+//   objects.push(new Object(x, y, vx, vy, m, r));
+// }
 
 function applyParameters() {
   parameterAttractionFroce = +document.getElementById("pAF").value;
   parameterAirFriction = +document.getElementById("pAf").value;
   parameterCollisonResponse = +document.getElementById("pCr").value;
   parameterViscosity = +document.getElementById("pV").value;
+  GravityCoeffcient = +document.getElementById("GC").value;
   return [
     parameterAttractionFroce,
     parameterAirFriction,
     parameterCollisonResponse,
     parameterViscosity,
+    GravityCoeffcient,
   ];
 }
-objects.push(new Object(100, 325, 10, 0, 20, 10));
-//objects.push(new Object(900, 325, -4, 0, 1, 1));
+objects.push(new Object(100, 325, 4, 0, 20, 10));
+objects.push(new Object(900, 325, -4, 0, 20, 10));
 // create Obj
 function Object(x, y, vx, vy, m, r) {
   this.x = x;
@@ -89,6 +91,7 @@ function Ff(m, v) {
     parameterAirFriction,
     parameterCollisonResponse,
     parameterViscosity,
+    GravityCoeffcient,
   ] = applyParameters();
   var F = 0;
 
@@ -113,7 +116,31 @@ function areColliding(obj1, obj2) {
   return distance <= obj1.r + obj2.r + 0.5;
 }
 
+function GF(obj1, obj2) {
+  [
+    parameterAttractionFroce,
+    parameterAirFriction,
+    parameterCollisonResponse,
+    parameterViscosity,
+    GravityCoeffcient,
+  ] = applyParameters();
+
+  var G =
+    (GravityCoeffcient * obj1.m * obj2.m) /
+    Math.sqrt((bhx - ballx) ** 2 + (bhy - bally) ** 2) ** 2;
+  var D = Math.sqrt((obj1.x - obj2.x) ** 2 + (obj1.y - obj2.y) ** 2);
+  return [G, D];
+}
+
 function update() {
+  [
+    parameterAttractionFroce,
+    parameterAirFriction,
+    parameterCollisonResponse,
+    parameterViscosity,
+    GravityCoeffcient,
+  ] = applyParameters();
+
   for (var i = 0; i < objects.length; i++) {
     objects[i].update();
     for (var j = i + 1; j < objects.length; j++) {
@@ -134,10 +161,10 @@ function update() {
         var v2fy =
           (Pinitialy - objects[i].m * (objects[j].vy - objects[i].vy)) /
           (objects[i].m + objects[j].m);
-        objects[i].vx = v1fx;
-        objects[i].vy = v1fy;
-        objects[j].vx = v2fx;
-        objects[j].vy = v2fy;
+        objects[i].vx = v1fx * parameterCollisonResponse;
+        objects[i].vy = v1fy * parameterCollisonResponse;
+        objects[j].vx = v2fx * parameterCollisonResponse;
+        objects[j].vy = v2fy * parameterCollisonResponse;
 
         var dx = objects[i].x - objects[j].x;
         var dy = objects[i].y - objects[j].y;
@@ -145,15 +172,15 @@ function update() {
         var overlap = objects[i].r + objects[j].r - distance;
         var miratio = objects[i].m / objects[j].m;
         var mjratio = objects[j].m / objects[i].m;
-        if (overlap > 1) {
-          console.log("1");
+        if (overlap > 2) {
           var angle = Math.atan2(dx, dy);
-          objects[i].x += (Math.sin(angle) * distance) / miratio;
-          objects[j].x -= (Math.sin(angle) * distance) / mjratio;
+          objects[i].x += (Math.sin(angle) * distance) / miratio + objects[i].r;
+          objects[j].x -= (Math.sin(angle) * distance) / mjratio + objects[j].r;
           objects[i].y += (Math.cos(angle) * distance) / miratio;
           objects[j].y -= (Math.cos(angle) * distance) / mjratio;
         }
       }
+      [G, D] = GF(objects[i], objects[j]);
     }
   }
 }
